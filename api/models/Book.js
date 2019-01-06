b59a46473a98c4735e6ad0a038731de8eb82e734
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2019-01-06 21:54:11 +0700

    add a new API for book routes

---
+++ api/models/Book.js
@@ -0,0 +1,50 @@
+/**
+ * Book.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  primaryKey: 'id',
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    id: {
+      type: 'number',
+      autoIncrement: true
+    },
+    name: {
+      type: 'string'
+    },
+    synopsis: {
+      type: 'string'
+    },
+    cover: {
+      type: 'string'
+    },
+    year: {
+      type: 'number'
+    },
+    author: {
+      type: 'string'
+    }
+
+  },
+
+};
+
